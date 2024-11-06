44c9a25ff917d7ac3f344ea3cc6b65fbffac4201
decokche <rmudi24@gmail.com>
2024-11-07 04:27:06 +0530

    backend url removed

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -12,6 +12,4 @@ export const handleError = (msg) => {
   })
 }
 
-export const APIUrl =
-  process.env.REACT_APP_API_URL || "http://localhost:8080"
-  
+export const APIUrl = process.env.REACT_APP_API_URL || "http://localhost:8080"
